105de7b5276a65e7034d70de7be5301cb5e2f7f2
Ajayi Olatunji O <ajayiolatunji17@gmail.com>
2025-05-28 23:48:53 +0100

    added logging interceptor

---
+++ libs/commonlib/src/interceptor/logger.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AppLoggerService } from './../logger';
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  constructor(private readonly logger: AppLoggerService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+
+    this.logger.setContext('LoggingInterceptor');
+    const now = Date.now();
+    return next.handle().pipe(
+      tap(() => {
+        const responseTime = Date.now() - now;
+        this.logger.log(
+          `Endpoint ${method} ${url} processed in ${responseTime}ms`,
+        );
+      }),
+    );
+  }
+}
