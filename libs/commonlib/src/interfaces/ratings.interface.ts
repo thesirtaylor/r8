f1f3a2d6be30d1a727dd1013f22ae51159372ccc
Ajayi Olatunji O <ajayiolatunji17@gmail.com>
2025-06-25 01:56:09 +0100

    updated interfaces to accomodate open api changes

--- libs/commonlib/src/interfaces/ratings.interface.ts
+++ libs/commonlib/src/interfaces/ratings.interface.ts
@@ -1,17 +1,39 @@
-export interface FindEntitysRatingsWithCursor {
+export interface IFindEntitysRatingsWithCursor {
   entityId: string;
   limit?: number;
+  cursor_id?: string;
+  cursor_createdAt?: string;
   cursor?: {
-    createdAt: Date;
-    id: string;
+    createdAt?: string;
+    id?: string;
   };
 }
 
-export interface CreateRating {
+export interface ICreateRating {
   userId: string;
   entityId: string;
   score: number;
   comment?: string;
   tags?: string[];
   anonymous?: boolean;
+}
+
+interface locationFilter {
+  city?: string;
+  state?: string;
+  country?: string;
+}
+
+export interface IGlobalRatingStats {
+  interval: 'day' | 'week' | 'month' | 'year';
+  cursor?: string;
+  limit?: number;
+  locationFilter?: locationFilter;
+  keyword?: string;
+  from?: string;
+  to?: string;
+}
+
+export interface IRatingStats {
+  id: string;
 }
