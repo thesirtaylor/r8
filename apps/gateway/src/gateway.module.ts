23c3b46d327b201195383a1c38270bb8e8a687de
Ajayi Olatunji O <ajayiolatunji17@gmail.com>
2025-07-08 10:48:12 +0100

    initialise API Gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { R8Module } from './r8/r8.module';
+import { SearchengineModule } from './searchengine/searchengine.module';
+import { AppDataSource, LoggerModule } from '@app/commonlib';
 import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './filters/exeptions.filters';
 
 @Module({
-  imports: [],
+  imports: [
+    R8Module,
+    SearchengineModule,
+    LoggerModule,
+    AuthModule,
+    TypeOrmModule.forRoot(AppDataSource.options),
+  ],
   controllers: [GatewayController],
-  providers: [GatewayService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
+  exports: [LoggerModule],
 })
 export class GatewayModule {}
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import {
+  AppDataSource,
+  AppLoggerService,
+  LoggingInterceptor,
+} from '@app/commonlib';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  await AppDataSource.initialize();
+  await AppDataSource.runMigrations();
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  const logger = app.get(AppLoggerService);
+
+  const apiConfig = new DocumentBuilder()
+    .setTitle('Api Gateway')
+    .setDescription('Api Gateway for r8')
+    .setVersion('0.1.0')
+    .addTag('rating')
+    .addApiKey(
+      {
+        type: 'apiKey',
+        name: 'x-access-token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, apiConfig);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalInterceptors(new LoggingInterceptor(logger));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(process.env.PORT_GATEWAY);
 }
 bootstrap();
