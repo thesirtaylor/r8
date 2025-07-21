880f0e4d021d2a1fbb0c23858a37d62e411b647a
Ajayi Olatunji O <ajayiolatunji17@gmail.com>
2025-07-21 18:26:56 +0100

    converting r8 to grpc microservice only, 3. users to grpc controllers

--- apps/r8/src/users/users.controller.ts
+++ apps/r8/src/users/users.controller.ts
@@ -1,22 +1,30 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { UserResponseDto } from '../../../gateway/src/openAPI';
+import { GrpcMethod } from '@nestjs/microservices';
+import {
+  GetUserRequest,
+  R8_SERVICE_NAME,
+  UserResponse,
+} from '@app/commonlib/protos_output/r8.pb';
+import { Observable } from 'rxjs';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Get('user')
-  // @ApiSecurity('access-token')
-  @ApiOperation({ summary: 'Fetch User by id' })
-  @ApiResponse({
-    status: 200,
-    description: 'OK',
-    type: UserResponseDto,
-  })
-  @ApiResponse({ status: 400, description: 'Bad Request' })
-  async GetUser(@Query('id') id: string) {
-    return this.usersService.findOneOrFail(id);
+  @GrpcMethod(R8_SERVICE_NAME, 'getUser')
+  async getUser(
+    payload: GetUserRequest,
+  ): Promise<UserResponse | Observable<UserResponse>> {
+    const { id } = payload;
+    const result = await this.usersService.findOneOrFail(id);
+    return {
+      id: result.id,
+      createdAt: result.createdAt.toDateString(),
+      updatedAt: result.updatedAt.toDateString(),
+      username: result?.username,
+      name: result.name,
+      email: result.email,
+      avatar: result?.avatar,
+    };
   }
 }
