c014341826fc482470920ad6689d9514497a72b9
Ajayi Olatunji O. <ajayiolatunji17@gmail.com>
2025-08-25 10:38:38 +0100

    removed redundant elasticsearch connection like in other servers

--- apps/searchengine/src/app.module.ts
+++ apps/searchengine/src/app.module.ts
@@ -1,28 +1,16 @@
-import { ConflictException, Module } from '@nestjs/common';
-import { AppDataSource, LoggerModule, HealthModule } from '@app/commonlib';
+import { Module } from '@nestjs/common';
+import { LoggerModule, HealthModule, dataSourceOptions } from '@app/commonlib';
 import { SearchengineModule } from './searchengine/searchengine.module';
-import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-const nodeUrl = process.env.ELASTICSEARCH_NODE;
-
-if (!nodeUrl) {
-  throw new ConflictException('ELASTICSEARCH_NODE not set');
-}
 
 @Module({
   imports: [
     LoggerModule,
     SearchengineModule,
-    TypeOrmModule.forRoot(AppDataSource.options),
+    TypeOrmModule.forRoot(dataSourceOptions),
     HealthModule,
-    HealthModule.register({
-      elasticsearchConfig: {
-        node: process.env.ELASTICSEARCH_NODE,
-      },
-    }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
