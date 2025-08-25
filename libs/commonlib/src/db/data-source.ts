f71d879839fc39a1f19dbd5efdee5bdba65eb44a
Ajayi Olatunji O. <ajayiolatunji17@gmail.com>
2025-08-25 10:47:44 +0100

    configure db lib to enable auto-migration

--- libs/commonlib/src/db/data-source.ts
+++ libs/commonlib/src/db/data-source.ts
@@ -1,19 +1,43 @@
-import { join } from 'path';
-import { Auth, Outbox, RateEntity, Rating, User } from '../entity';
-import { DataSource } from 'typeorm';
+import { resolve } from 'path';
+import {
+  Auth,
+  Outbox,
+  TheEntity,
+  Rating,
+  User,
+  MediaOutbox,
+  Media,
+  MediaBatch,
+} from '../entity';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const runningTs = __filename.endsWith('.ts');
+const MIG_TS = resolve(__dirname, '../migrations/*.ts');
+const MIG_JS = resolve(__dirname, '../migrations/*.js');
+
+export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [User, RateEntity, Rating, Outbox, Auth],
-  migrations: [join(__dirname, 'apps/migrations/*.{ts,js}')],
+  entities: [
+    User,
+    TheEntity,
+    Rating,
+    Outbox,
+    Auth,
+    MediaOutbox,
+    Media,
+    MediaBatch,
+  ],
+  migrations: [runningTs ? MIG_TS : MIG_JS],
   synchronize: true,
   // logging: !!process.env.DB_LOG,
-});
+};
+
+export const AppDataSource = new DataSource(dataSourceOptions);
